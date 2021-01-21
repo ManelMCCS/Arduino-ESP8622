52ad236018c08979564e500bbd77b1cbe2ef8994
ManelMCCS <67638640+ManelMCCS@users.noreply.github.com>
2021-01-21 22:11:38 +0000

    Update main.cpp
    Ainda não foi testado com o ESP

--- main.cpp
+++ main.cpp
@@ -1,28 +1,43 @@
 #include <Arduino.h>
 
-#define RL 3//PT:Pino do LED vermelho;  EN:Red LED pin;
-#define YL 2//PT:Pino do LED amarelo;  EN:Yellow LED pin;
-#define GL 1//PT:Pino do LED verde;  EN:Green LED pin;
+#include <SerialESP8266wifi.h>
+#include <WiFiClient.h>
+#include "index.h" // guarda o html
 
-#define B_IN 6//PT:Pino para o butão de entrada;  EN: Entry room button pin;
-#define B_OUT 5//PT:Pino para o butão de saída;  EN:Leave room button pin;
+String s = MAIN_page; // guarda o codigo para ser enviado para o browser
+
+//const char s = MAIN_page;
+
+//Dados do router
+const char* ssid;
+const char* password;
+
+#define RL 2//PT:Pino do LED vermelho; 
+#define YeL 3//PT:Pino do LED amarelo;  
+#define GL 4//PT:Pino do LED verde;  
+
+#define B_IN 11//PT:Pino para o butão de entrada;  
+#define B_OUT 6//PT:Pino para o butão de saída;  
 
 
-float room_size; //PT:Dimensões da sala em m^2;  EN:Dimensions of the room in m^2;
-float pm2; //PT:Número de pessoas por metro quadrado;  EN:Number of people per square meter;
+float room_size; //PT:Dimensões da sala em m^2; 
+float pm2; //PT:Número de pessoas por metro quadrado;  
 
-//PT: Valores predifinidos para o pm2; EN:pm2 defalt values
+//PT: Valores predifinidos para o pm2;
 #define muito_afastado 0.05 
 #define afastado 0.6
 #define proximo 1
 
-//(Ponto n.º 4 do Despacho n.º 8998-C/2020, de 18 de setembro e artigos 7.º  n.º 1 a) e b) da RCM 70-A/2020):
 
 unsigned int room_capacity;
 unsigned int n;
 unsigned int max;
-unsigned float perc;
-unsigned float dist;
+float perc;
+float dist;
+
+
+int time = millis();
+int refresh_rate = 1000;
 
 
 /*
@@ -30,11 +45,6 @@ PT:
 Retorna o número máximo de pessoas que podem estar em uma sala.
 Esta função leva em consideração as dimensões da sala em m^2 (room_size) e o número máximo de pessoas por metro quadrado (pm2).
 Por razões práticas, o resultado é arredondado para baixo por padrão.
-
-EN:
-Returns the maximum number of people that can be in a room.
-This function takes into account the dimensions of the room in m^2 (room_size) and the maximum number of people per square meter (pm2).
-For practical reasons the result is rounded down by default.
 */
 int capacity(float room_size, float pm2) {return (int)room_size*pm2;}
 
@@ -42,69 +52,176 @@ void LEDS(float perc)
 {
   if (perc < 33)
   {
-    digitalWrite(YL, LOW);
+    digitalWrite(YeL, LOW);
     digitalWrite(GL, HIGH);
   }
   else  if (perc < 66)
   {
     digitalWrite(RL, HIGH);
-    digitalWrite(YL, LOW);
+    digitalWrite(YeL, LOW);
     digitalWrite(GL, LOW);
   }
   else
   {
-    digitalWrite(YL, LOW);
+    digitalWrite(YeL, LOW);
     digitalWrite(GL, HIGH); 
   }
 }
 
-void nHandler(int n) 
-{
-  server.send(200, "text/plane", String(n)); //Send ADC value only to client ajax request
-}
-
-float webInputHandler(String query)
-{
-  String str_value = server.arg(query); //Refer  xhttp.open("GET", id +"?q=" + getValue(id), true);
-  Serial.println(str_value);
-  if(t_state == "1")
-}
-
-void setup() 
+void GPIO_SETUP()
 {
   pinMode(RL, OUTPUT);
-  pinMode(YL, OUTPUT);
+  pinMode(YeL, OUTPUT);
   pinMode(GL, OUTPUT);
   
   pinMode(B_IN, INPUT_PULLUP);
   pinMode(B_OUT, INPUT_PULLUP);
-
+  
+  //teste de LEDS
+  digitalWrite(RL, HIGH);
+  digitalWrite(YeL, HIGH);
+  digitalWrite(GL, HIGH);
+  delay(1000);
   digitalWrite(RL, LOW);
-  digitalWrite(YL, LOW);
+  digitalWrite(YeL, LOW);
   digitalWrite(GL, LOW);
+}
 
+void htmlHandler() 
+{
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Envia webpage para o cliente AJAX
+}
+
+void nHandler(int n) 
+{
+  server.send(200, "text/plane", String(n)); //Envia número de pessoas para o cliente AJAX
+}
+
+void webInputHandler(String query, float* value)
+{
+  String str_value = server.arg(query); //xhttp.open("GET", id +"?q=" + getValue(id), true);
+  Serial.println(str_value);
   
+  *value = (int)str_value;
+
+  return value;
+}
+
+void SendToESP(String command, const int timeout, boolean debug)
+{
+  // Envio dos comandos AT para o modulo
+  String resposta = "";
+  Serial.print(command);
+  //Serial1.print(command);
+  long int time = millis();
+  while ( (time + timeout) > millis())
+  {
+    while (Serial1.available())
+    {
+      char c = Serial1.read();
+      resposta += c;
+    }
+  }
+  if (debug) Serial.println(resposta);
+}
+
+void CheckESP()
+{
+  Serial.println("Verifica conexão");
+  SendToESP("AT\r\n", 2000, 1);
   
-  pm2 = muito_afastado;
+  delay(1000);
+  
+  Serial.println("Versão Firmware");
+  SendToESP("AT+GMR\r\n", 2000, 1);
+  Serial.println();
+}
+
+void conectarWIFI(int tentativas)
+{
+  ESP8266WebServer server(80); //abre servidor na porta 80 (https)
+  
+  int n = 0;
+  
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+    if (n > tentativas) break;
+    n++;
+  }
+
+  //Informações do IP da rede
+  
+  Serial.println("Rede: ");
+  Serial.println(ssid);
+  Serial.println("");
+  Serial.println("");
+  Serial.println("")("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void IniciarServidor(int n, float *room_size, float *pm2)
+{
+  //Envia pedidos AJAX
+  
+  server.on("/", htmlHandler()); //envia pagina web page
+  server.on("/numb", nHandler(n)); //envia dados do número de pessoas
+  server.on("/tam", webInputHandler("TamSala", &room_size);  //recebe dados; SendValue('tam', 'TamSala')
+  server.on("/pm2_max",  webInputHandler("np")); //recebe dados; SendValue('pm2_max', 'np')
+
+  server.begin(); // inicializa o servidor
+  Serial.println("HTTP server started");
+}
+
+
+
+void setup() 
+{
+  GPIO_SETUP();
+
+  Serial.begin(9600); //comunicação UART com o computador
+  Serial1.begin(115200); //comunicação UART com o ESP8266
+  void CheckESP();
+
+  conectarWIFI(10);
+  IniciarServidor();
+
   max = 0;
   n  = 0;
 
+  while(!max) //espera o input do utilizador no browser
+  {
+    server.handleClient(); //Cliente AJAX para o troca de informações
+    max = capacity(room_size, pm2);
+  }
 }
 
 void loop() 
 {
-  room_size = 55; //REQUEST AO ESP
-  pm2 = 1; //REQUEST AO ESP entre valores costumizados e predefinidos
-
-  if(!max) max = capacity(room_size, pm2);
+  if (millis() - time > refresh_rate) 
+  {
+    Serial.println("Valor de n:");
+    Serial.println(n);  
+  
+    Serial.println("Maximo de pessoas");
+    Serial.println(max); 
+    
+    perc = ((float)n/(float)max)*100;
+    Serial.println("Percentagem de ocupação:");
+    Serial.println(perc);
+    
+    time += refresh_rate;
+  }
 
   if (n < max && digitalRead(B_IN)) n++;
   if (n > 0 && digitalRead(B_OUT)) n--;
+  
+  server.handleClient(); //Cliente AJAX para o troca de informações
 
-  perc = (float)(n/max)*100;
-  dist = n/room_size;
-
-  LEDS(perc);
-
-  server.on("/numb", percHandler(n));
 }
